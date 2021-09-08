a308b6775c709b3b3dbf8cba8b2cb58e079fb8d5
Daniel Morris <dmorris0@users.noreply.github.com>
2021-09-08 09:05:09 -0400

    Update arithmetic.py

--- Python/arithmetic.py
+++ Python/arithmetic.py
@@ -13,11 +13,13 @@ def sum_to_n(n):
     return sum_val
 
 if __name__ == "__main__":
-    # The above line allows this file to do double duty.  
+    # The above condition: __name__=="__main__", allows this file to do double duty.  
     # 1) It can act as a package defining functions or class definitions above that can be imported by other python files
-    #    In this case what is below is ignored.
+    #    When functions or classes are imported, what is after this condition is ignored.
     # 2) This file can run in a debugger or called directly using: python filename.py
-    #    In this case all the classes / functions above are defined, and then the code below is executed.
+    #    In this case all the classes / functions above are defined, and then the code below is also executed.
+    #    This is convenient for testing and debugging functions defined above.  In this case call the following from the command line:
+    #    python arithmetic.py
     
     a, b = 3, 4
     print(f"Testing multiply({a},{b}) = {multiply(a,b)}")
